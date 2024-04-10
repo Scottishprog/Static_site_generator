bcc571cc30945e83c118a8f0eed18f2487e83397
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2024-04-09 20:32:25 -0700

    Finalized the recursive genration function. This is the likely end of the project.

--- src/gencontent.py
+++ src/gencontent.py
@@ -23,20 +23,26 @@ def generate_page(from_path, template_path, dest_path):
     title_text = extract_title(markdown_value)
     template_value = template_value.replace('{{ Title }}', title_text)
     template_value = template_value.replace('{{ Content }}', content_html)
-    dest_dirname = os.path.dirname(from_path)
+    dest_dirname = os.path.dirname(dest_path)
     if not os.path.exists(dest_dirname):
+        print(f'Making directory: {dest_dirname}')
         os.makedirs(dest_dirname)
     with open(dest_path, 'w') as write_file:
         write_file.write(template_value)
 
 
-def gen_content_recursive(from_path, template_path, dest_path):
-    for path in os.listdir(from_path):
-        if os.path.isfile(from_path):
-            generate_page(os.path.join(from_path, path),
+def generate_page_recursive(dir_path_content, template_path, dest_path):
+    for path in os.listdir(dir_path_content):
+        source_file_path = os.path.join(dir_path_content, path)
+        if os.path.isfile(source_file_path):
+            file_name = path.split('.')
+            file_name = file_name[0]
+            file_name = file_name + '.html'
+            dest_file_path = os.path.join(dest_path, file_name)
+            generate_page(source_file_path,
                           template_path,
-                          os.path.join(dest_path))
-        if os.path.isdir(path):
-            gen_content_recursive(os.path.join(from_path, path),
-                                  template_path,
-                                  os.path.join(dest_path, path))
+                          dest_file_path)
+        else:
+            generate_page_recursive(os.path.join(dir_path_content, path),
+                                    template_path,
+                                    os.path.join(dest_path, path))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from copystatic import copy_tree
-from gencontent import generate_page
+from gencontent import generate_page_recursive
 
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -22,7 +22,8 @@ def main():
     print(f'Copying files from {path_source} to {path_dest}...')
     copy_tree(path_source, path_dest)
 
-    generate_page(os.path.join(path_content, 'index.md'), path_template, os.path.join(path_dest, 'index.html'))
+    # generate_page(os.path.join(path_content, 'index.md'), path_template, os.path.join(path_dest, 'index.html'))
+    generate_page_recursive(path_content, path_template, path_dest)
 
 
 main()
